957af4341bd13c8df14986965bf5e007faaa69a6
Cruelkid <sonofammit@gmail.com>
2021-09-25 23:33:36 +0300

    added nested route

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,5 +1,15 @@
+import { Fragment } from 'react';
+import { Route } from 'react-router';
+
 const WelcomePage = () => {
-    return <h1>Welcome page</h1>;
+    return (
+        <Fragment>
+            <h1>Welcome page</h1>
+            <Route path="/welcome/new-user">
+                <p>Welcome, new user!</p>
+            </Route>
+        </Fragment>
+    );
 };
 
 export default WelcomePage;
