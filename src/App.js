cde4284202e4d5327e573067540c9843b6fac52d
Cruelkid <sonofammit@gmail.com>
2021-09-25 23:07:48 +0300

    added header component, added react router link

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { Route } from 'react-router';
+import MainHeader from './components/MainHeader';
 import ProductsPage from './pages/ProductsPage';
 import WelcomePage from './pages/WelcomePage';
 
 function App() {
     return (
         <div>
-            <Route path="/welcome">
-                <WelcomePage />
-            </Route>
-            <Route path="/products">
-                <ProductsPage />
-            </Route>
+            <MainHeader />
+            <main>
+                <Route path="/welcome">
+                    <WelcomePage />
+                </Route>
+                <Route path="/products">
+                    <ProductsPage />
+                </Route>
+            </main>
         </div>
     );
 }
